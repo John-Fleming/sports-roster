a2175a5e33acfebf283be87b7a0bedade3ab17c3
John Fleming <johndeclanfleming@gmail.com>
2020-05-18 20:25:53 -0500

    added initial components

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Navbar.scss';
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <h2>Navbar component</h2>
+    );
+  }
+}
+
+export default Navbar;
