bebf7911bba9bfbd349b45aa3c80f866c36963e8
John Fleming <johndeclanfleming@gmail.com>
2020-05-20 19:15:05 -0500

    set roster state with players by UID

--- src/components/Roster/Roster.js
+++ src/components/Roster/Roster.js
@@ -2,11 +2,25 @@ import React from 'react';
 
 import './Roster.scss';
 
+import playerData from '../../helpers/data/playerData';
+import authData from '../../helpers/data/authData';
+
 class Roster extends React.Component {
+  state = {
+    players: [],
+  }
+
+  componentDidMount() {
+    console.error('uid', authData.getUid());
+    playerData.getPlayersByUid(authData.getUid())
+      .then((players) => this.setState({ players }))
+      .catch((err) => console.error('could not get players: ', err));
+  }
+
   render() {
     return (
       <div className="Roster">
-        <h2>In Roster Component</h2>
+        <h2>Astros' Roster</h2>
       </div>
     );
   }
