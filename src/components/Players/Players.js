003118a4d3b499122808605bc07b3ddfa23fb511
John Fleming <johndeclanfleming@gmail.com>
2020-05-22 17:19:52 -0500

    minor style edits to nav and cards

--- src/components/Players/Players.js
+++ src/components/Players/Players.js
@@ -10,9 +10,12 @@ class Player extends React.Component {
   render() {
     const { player } = this.props;
     return (
-      <div className="Player col-md-4 my-2">
-        <div className="card">
-        <img className="card-img-top player-photo" src={player.imageUrl} alt="Player" />
+      <div className="Player col-lg-4 col-md-6 my-2">
+        <div className="card mx-auto">
+          <div className="header-photo-container">
+            <img className="card-img-top header-photo" src="https://upload.wikimedia.org/wikipedia/commons/0/01/Houston_astros_logo.png" alt="Player" />
+          </div>
+        <img className="player-photo" src={player.imageUrl} alt="Player" />
         <div className="card-body">
           <h5 className="card-title">{player.name}</h5>
           <p className="card-text">{player.position}</p>
